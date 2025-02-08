ab02efd1c9e3610e7bf35d43265a4fbdffecdac2
Aarush Pawar <aarush711pawar@gmail.com>
2025-02-08 18:52:42 -0500

    gemini fix

--- src/components/Gemini.tsx
+++ src/components/Gemini.tsx
@@ -5,7 +5,7 @@ import { ChatSession, GoogleGenerativeAI, StartChatParams } from "@google/genera
 import APIKey from './GeminiAPIKey';
 
 const googleAI = new GoogleGenerativeAI(APIKey);
-const model = googleAI.getGenerativeModel({ model: "gemini-2.0-pro" });
+const model = googleAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 
 function startChat() {
   let chat = model.startChat();
